d5cb13ac68e83821a7621b985f84e5aab36602af
ntu_ME_Fianl project <lawcom333@gmail.com>
2025-05-24 00:11:56 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
 import streamlit as st
+import json
 
-# 讀取 Google Sheet 名稱
 SPREADSHEET_NAME = "pin_data"
-
-# 讀取 OpenRouteService 的 API 金鑰（從 secrets 裡）
 ORS_API_KEY = st.secrets["ORS_API_KEY"]
-
-# 讀取 Google 憑證（從 secrets 裡）
-GCP_CREDENTIALS = dict(st.secrets["gcp_service_account"])
+GCP_CREDENTIALS = json.loads(st.secrets["GCP_CREDENTIALS"])
