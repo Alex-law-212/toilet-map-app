7611f098a98a027190cdcb3b00179eab655ff45b
Kevin646556 <kevin0965356169@gmail.com>
2025-05-26 17:21:51 +0800

    Update location.py

--- location.py
+++ location.py
@@ -2,6 +2,6 @@ from streamlit_geolocation import streamlit_geolocation
 
 def get_user_location():
     location = streamlit_geolocation()
-    if location and location.get("latitude") and location.get("longitude"):
+    if location and location.get("latitude") is not None and location.get("longitude") is not None:
         return (location["latitude"], location["longitude"])
     return None
